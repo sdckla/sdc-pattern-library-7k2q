c97b6fbe39f426c24a72ea20a963f28da57ee08b
sdckla <sdckla@jerrybag.com>
2026-09-25 10:23:33 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Pattern Library settings (Supabase project: SDC Pattern Library)
+// The publishable key is meant to be public. Never put a secret key (sb_secret_...) here.
+window.PATTERN_CONFIG = {
+  supabaseUrl: 'https://guyjvxragyptetnapspw.supabase.co',
+  supabaseKey: 'sb_publishable_21R9RG81pCD9RvBSHQdGEg_9aAbug9z',
+};
